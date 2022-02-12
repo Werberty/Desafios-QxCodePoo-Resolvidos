b120150807a28900e81c952efbe7a5d636bd2db2
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-12 10:09:23 -0300

    adicionado o metodo de verificar passsageiro
    Ainda está inacabado

--- Desafios/topic_luxo.py
+++ Desafios/topic_luxo.py
@@ -1,5 +1,4 @@
 from abc import ABC
-import enum
 
 class Passageiro(ABC):
     def __init__(self, nome, idade) -> None:
@@ -16,7 +15,7 @@ class Passageiro(ABC):
 
 
 class Topic:
-    def __init__(self, lotacao_max, preferenciais) -> None:
+    def __init__(self, lotacao_max, preferenciais):
         self._lotacao_max = lotacao_max
         self._preferenciais = preferenciais
         self._cadeiras = list(f"{preferenciais*'@'}{(lotacao_max-preferenciais)*'='}")
@@ -26,20 +25,32 @@ class Topic:
         return self._cadeiras
     
     def inserir_passageiro(self, nome, idade):
-        passageiro = Passageiro(nome, idade)
-        for n, cadeira in enumerate(self.cadeiras):
-            if passageiro.idade >= 60:
-                if cadeira == '@':
-                    self.cadeiras[n] += passageiro.nome
-                    return
-                elif cadeira == '=':
-                    self.cadeiras[n] += passageiro.nome
-                    return
-            else:
-                if cadeira == '=':
-                    self.cadeiras[n] += passageiro.nome
-                    return
+        verificador = self.verificar_passageiro(nome)
+        if verificador:
+            passageiro = Passageiro(nome, idade)
+            for n, cadeira in enumerate(self.cadeiras):
+                if passageiro.idade >= 60:
+                    if cadeira == '@':
+                        self.cadeiras[n] += passageiro.nome
+                        return
+                    elif cadeira == '=':
+                        self.cadeiras[n] += passageiro.nome
+                        return
+                else:
+                    if cadeira == '=':
+                        self.cadeiras[n] += passageiro.nome
+                        return
     
+    def verificar_passageiro(self, nome): #verificar possiveis problemas ainda
+        verificador = True
+        for cadeira in self.cadeiras:
+            cadeira = cadeira.replace('@', '')
+            cadeira = cadeira.replace('=', '')
+            if nome == cadeira:
+                print('Passageiro ja existe')
+                verificador = False
+        return verificador
+
     def mostrar_cadeiras(self):
         print('[',end=' ')
         for c in self.cadeiras:
@@ -48,11 +59,9 @@ class Topic:
 
 
 topic = Topic(5, 2)
-topic.inserir_passageiro('betim', 67)
-topic.inserir_passageiro('Bia', 45)
-topic.inserir_passageiro('ana', 17)
-topic.inserir_passageiro('Luiz', 90)
-topic.inserir_passageiro('Antonio', 109)
-topic.inserir_passageiro('Cheila', 7)
+topic.inserir_passageiro('davi', 17)
+topic.inserir_passageiro('joao', 103)
+topic.inserir_passageiro('ana', 35)
+topic.inserir_passageiro('rex', 20)
+topic.inserir_passageiro('bia', 16) #não está adicionando a bia
 topic.mostrar_cadeiras()
-
