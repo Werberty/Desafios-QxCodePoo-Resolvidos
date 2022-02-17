fcfeee4afb3649dee60960f204abbee0a08e26f4
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-17 09:11:08 -0300

    adicionando métodos de agregar e checar

--- Desafios/sistemaBancario/banco.py
+++ Desafios/sistemaBancario/banco.py
@@ -2,3 +2,17 @@ class banco:
     def __init__(self) -> None:
         self.contas = []
         self.clientes = []
+    
+    def agregar(self, conta, cliente):
+        self.contas = self.contas.append(conta)
+        self.clientes = self.clientes.append(cliente)
+    
+    def checar(self, cliente, conta):
+        verificador = False
+        for cont in self.contas:
+            if cont == conta:
+                verificador = True
+        for cli in self.clientes:
+            if cli == cliente:
+                verificador = True
+        return verificador
