bb9be29140890a561dd0bbbdb8d607c5768eb556
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-25 10:41:21 -0300

    inicio do desafio

---
+++ Desafios/contato_telefonico.py
@@ -0,0 +1,5 @@
+'''Contato Telefônico
+O objetivo dessa atividade é implementar uma classe responsável 
+por guardar um único contato da agenda telefônica do seu celular. 
+Cada contato pode ter vários telefones.
+'''
