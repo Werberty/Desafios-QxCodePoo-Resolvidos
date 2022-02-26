6c4e09f4c8f0bfab1c35ef31ad6198b9bb9d1fda
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-26 15:41:46 -0300

    Funcionalidade incial

--- Desafios/contato_telefonico.py
+++ Desafios/contato_telefonico.py
@@ -3,3 +3,13 @@ O objetivo dessa atividade é implementar uma classe responsável
 por guardar um único contato da agenda telefônica do seu celular. 
 Cada contato pode ter vários telefones.
 '''
+
+class Contato:
+    def __init__(self, nome='Vazio') -> None:
+        self.nome = nome
+
+
+if __name__ == '__main__':
+    c1 = Contato('Werberty')
+    print(c1.nome)
+
