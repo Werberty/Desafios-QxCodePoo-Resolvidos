ccb255736298c451e732ef264c453fe79e5dedcb
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-26 16:45:14 -0300

    implementando inserir telefones no contato

--- Desafios/contato_telefonico.py
+++ Desafios/contato_telefonico.py
@@ -5,11 +5,21 @@ Cada contato pode ter vários telefones.
 '''
 
 class Contato:
-    def __init__(self, nome='Vazio') -> None:
-        self.nome = nome
-
+    def __init__(self, nome:str='Vazio') -> None:
+        self._nome = nome
+        self._telefone = {}
+        
+    def adicionar_telefone(self, label: str, telefone: str):
+        self._telefone[label] = telefone
+    
+    def show(self):
+        for n, (label, tele) in enumerate(self._telefone.items()):
+            print(f'[{n}:{label}:{tele}]', end='')
+        print()
 
 if __name__ == '__main__':
     c1 = Contato('Werberty')
-    print(c1.nome)
+    c1.adicionar_telefone('oi', '99')
+    c1.adicionar_telefone('casa', '11')
+    c1.show()
 
