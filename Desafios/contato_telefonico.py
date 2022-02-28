cb54b743e0eb6a67566416a2038789409375d1b2
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-28 10:35:35 -0300

    Finalizando implementando um validador de telefone

--- Desafios/contato_telefonico.py
+++ Desafios/contato_telefonico.py
@@ -3,13 +3,20 @@ O objetivo dessa atividade é implementar uma classe responsável
 por guardar um único contato da agenda telefônica do seu celular. 
 Cada contato pode ter vários telefones.
 '''
+import re
+
 class Telefone:
     def __init__(self, label, numero) -> None:
         self._label = label
         self._numero = numero
     
-    def validar(self):
-        pass
+    def validar_fone(self, telefone):
+        somente_num = re.sub("\(|\)|\ |\-|\.", "", telefone)
+        print(somente_num)
+        if not somente_num.isnumeric():
+            return False
+        
+        return True
 
 
 class Contato:
@@ -17,9 +24,13 @@ class Contato:
         self._nome = nome
         self._telefones = []
         
-    def adicionar_telefone(self, label: str, telefone: str):
-        self._telefones.append(Telefone(label, telefone))
-    
+    def adicionar_telefone(self, label: str, numero: str):
+        telefone = Telefone(label, numero)
+        if telefone.validar_fone(numero):
+            self._telefones.append(telefone)
+        else:
+            print('Fail: fone inválido')
+        
     def remover_telefone(self, indice):
         del self._telefones[indice]
     
@@ -28,8 +39,10 @@ class Contato:
         self._telefones = []
         for fone in args:
             label, num = fone
-            self._telefones.append(Telefone(label, num))
-    
+            telefone = Telefone(label, num)
+            if telefone.validar_fone(num):
+                self._telefones.append(telefone)
+        
     def show(self):
         for n, fone in enumerate(self._telefones):
             print(f'[{n}:{fone._label}:{fone._numero}]', end='')
@@ -40,6 +53,6 @@ if __name__ == '__main__':
     c1.adicionar_telefone('oi', '99')
     c1.adicionar_telefone('casa', '11')
     c1.remover_telefone(1)
-    c1.atualizar_contato('Bety', ('tim', '9087'), ('fixo', '11021'))
+    c1.atualizar_contato('Bety', ('tim', '(11) 9087'), ('fixo', '110a21'))
     c1.show()
 
