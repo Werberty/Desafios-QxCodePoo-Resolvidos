5cd6d46357051562e1b573f719142243676dd2c6
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-28 20:09:33 -0300

    Finalizado

--- Desafios/contato_telefonico.py
+++ Desafios/contato_telefonico.py
@@ -5,35 +5,35 @@ Cada contato pode ter vários telefones.
 '''
 import re
 
+
 class Telefone:
     def __init__(self, label, numero) -> None:
         self._label = label
         self._numero = numero
-    
+
     def validar_fone(self, telefone):
         somente_num = re.sub("\(|\)|\ |\-|\.", "", telefone)
-        print(somente_num)
         if not somente_num.isnumeric():
             return False
-        
+
         return True
 
 
 class Contato:
-    def __init__(self, nome:str='Vazio') -> None:
+    def __init__(self, nome: str = 'Vazio') -> None:
         self._nome = nome
         self._telefones = []
-        
+
     def adicionar_telefone(self, label: str, numero: str):
         telefone = Telefone(label, numero)
         if telefone.validar_fone(numero):
             self._telefones.append(telefone)
         else:
             print('Fail: fone inválido')
-        
+
     def remover_telefone(self, indice):
         del self._telefones[indice]
-    
+
     def atualizar_contato(self, nome, *args):
         self._nome = nome
         self._telefones = []
@@ -42,17 +42,18 @@ class Contato:
             telefone = Telefone(label, num)
             if telefone.validar_fone(num):
                 self._telefones.append(telefone)
-        
+
     def show(self):
         for n, fone in enumerate(self._telefones):
             print(f'[{n}:{fone._label}:{fone._numero}]', end='')
         print()
+
 
 if __name__ == '__main__':
     c1 = Contato('Werberty')
     c1.adicionar_telefone('oi', '99')
     c1.adicionar_telefone('casa', '11')
     c1.remover_telefone(1)
-    c1.atualizar_contato('Bety', ('tim', '(11) 9087'), ('fixo', '110a21'))
+    c1.atualizar_contato('Bety', ('tim', '(11) 9087'),
+                         ('fixo', '110a21'), ('claro', '94-96'))
     c1.show()
-
